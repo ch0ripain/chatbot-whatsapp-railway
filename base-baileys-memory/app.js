053642c59539db83103a3d608f70c7e39e9799df
Leandro Rufino <leandrix1234@gmail.com>
2025-04-09 00:42:40 -0300

    tracking habits implemented using googleapis_googlesheets

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -1,4 +1,4 @@
-// const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require('@bot-whatsapp/bot')
+// const { createBot, createProvider, createFlow, addKeyword, EVENTS } = require(@bot-whatsapp/bot')
 import pkg from '@bot-whatsapp/bot'
 const { createBot, createProvider, createFlow, addKeyword, EVENTS } = pkg
 // require("dotenv").config()
@@ -21,6 +21,7 @@ import { delay } from '@adiwajshing/baileys'
 import { generarImagen } from './lcm-lora.js'
 import { assemblyAI } from './assembly-ai.js'
 import { huggingFace } from './huggingface.js'
+import { appendToSheet, readSheet } from './google-sheets.js'
 
 const __dirname = import.meta.dirname
 
@@ -52,29 +53,84 @@ const flowMenu = addKeyword(['menu', 'menú', 'Menú'])
         }
     })
 
-const flowEvento = addKeyword('evento')
+const flowTracker = addKeyword('tracker')
     .addAction(async (ctx, ctxFn) => {
         const isMessageFromGroup = !!ctx.message.extendedTextMessage
-        const mensajeEvento = 'Esta función todavía no está disponible ⏳'
-        'Esta función todavía no está disponible ⏳'
         if (isMessageFromGroup) {
-            await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, mensajeEvento)
+            await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, 'Por el momento, solo se pueden registrar datos por privado 😞')
+            await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, 'Recuerda que puedes consultar las actividades con *Track* 🧠📲')
+            await ctxFn.flowDynamic('Intenta por acá 🤠')
+            return
         } else {
-            await ctxFn.flowDynamic(mensajeEvento)
+            return await ctxFn.gotoFlow(flowTrackerRegistro)
         }
     })
 
-
-const flowProgreso = addKeyword('progreso')
+const flowTrackerRegistro = addKeyword(EVENTS.ACTION)
+    .addAnswer('Dame una descripción breve de la actividad que quieras guardar 📝', { capture: true }, async (ctx, ctxFn) => {
+        await ctxFn.state.update({ nombre: ctx.pushName, descripcion: ctx.body, estado: 'Pendiente 🟡' })
+    })
+    .addAnswer('¿Cuál es la fecha límite para completarlo? 📅⏳', { capture: true }, async (ctx, ctxFn) => {
+        await ctxFn.state.update({ fecha: ctx.body })
+    })
     .addAction(async (ctx, ctxFn) => {
-        const isMessageFromGroup = !!ctx.message.extendedTextMessage
-        const mensajeProgreso = 'Esta función todavía no está disponible ⏳'
-        if (isMessageFromGroup) {
-            await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, mensajeProgreso)
-        } else {
-            await ctxFn.flowDynamic(mensajeProgreso)
+        const nombre = ctxFn.state.get("nombre")
+        const descripcion = ctxFn.state.get("descripcion")
+        const estado = ctxFn.state.get("estado")
+        const fecha = ctxFn.state.get("fecha")
+
+        const respuesta = await appendToSheet([nombre, descripcion, fecha, estado])
+
+        if (!respuesta || respuesta.statusText !== 'OK') {
+            await ctxFn.flowDynamic('Ups… algo salió mal. Intenta de nuevo más tarde! ⏳')
         }
     })
+    .addAnswer(['Actividad registrada ✅'])
+    .addAnswer(['Recuerda que puedes consultar las actividades con *Track* 🧠📲'])
+
+const flowTrackerVer = addKeyword('track')
+    .addAction(async (ctx, ctxFn) => {
+        const mensajeInicial = 'Consultando actividades pendientes... 🧠'
+        const isMessageFromGroup = !!ctx.message.extendedTextMessage
+
+        if (isMessageFromGroup) {
+            await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, mensajeInicial)
+        } else {
+            await ctxFn.flowDynamic(mensajeInicial)
+        }
+
+        const datos = await readSheet("Rutatina!A2:D")
+
+        if (!datos || datos.length === 0) {
+            const mensajeError = 'Todavía no hay datos registrados 😔'
+            if (isMessageFromGroup) {
+                return await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, mensajeError)
+            } else {
+                return await ctxFn.flowDynamic(mensajeError)
+            }
+        }
+
+        // Agrupar por nombre
+        const agrupado = {}
+        datos.forEach(([nombre, descripcion, fecha, estado]) => {
+            if (!agrupado[nombre]) {
+                agrupado[nombre] = []
+            }
+            agrupado[nombre].push(`🟡 ➡️ ${descripcion} ➡️ ${fecha} ⏱️`)
+        })
+
+        // Crear mensajes para cada grupo
+        for (const nombre in agrupado) {
+            const actividades = agrupado[nombre].join('\n')
+            const mensaje = `Actividades de ${nombre}\n${actividades}`
+            if (isMessageFromGroup) {
+                await ctxFn.provider.sendText(process.env.WHATSAPP_GROUP_ID, mensaje)
+            } else {
+                await ctxFn.flowDynamic(mensaje)
+            }
+        }
+    })
+
 
 const flowFraseAleatoria = addKeyword('frase')
     .addAction(async (ctx, ctxFn) => {
@@ -211,12 +267,13 @@ const flowSalir = addKeyword(['salir', 'chau', 'nv', 'gracias'])
         }
     })
 
+
 const main = async () => {
     const adapterDB = new MongoAdapter({
         dbUri: process.env.MONGO_DB_URI,
         dbName: "MaruBot"
     })
-    const adapterFlow = createFlow([flowWelcome, flowMenu, flowEvento, flowProgreso, flowFraseAleatoria, flowDatoCurioso, flowImagen, flowAudio, flowDescribirImagen, flowImagenATexto, flowSalir,])
+    const adapterFlow = createFlow([flowWelcome, flowMenu, flowTracker, flowTrackerRegistro, flowTrackerVer, flowFraseAleatoria, flowDatoCurioso, flowImagen, flowAudio, flowDescribirImagen, flowImagenATexto, flowSalir,])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
---
+++ base-baileys-memory/google-sheets.js
@@ -0,0 +1,45 @@
+// const { google } = require('googleapis');
+import { google } from "googleapis";
+// Initializes the Google APIs client library and sets up the authentication using service account credentials.
+const auth = new google.auth.GoogleAuth({
+    keyFile: './google.json',  // Path to your service account key file.
+    scopes: ['https://www.googleapis.com/auth/spreadsheets']  // Scope for Google Sheets API.
+});
+
+export async function appendToSheet(values) {
+
+    const spreadsheetId = process.env.SPREADSHEET_ID
+    const sheets = google.sheets({ version: 'v4', auth }); // Create a Sheets API client instance
+    const range = 'Rutatina!A1'; // The range in the sheet to start appending
+    const valueInputOption = 'USER_ENTERED'; // How input data should be interpreted
+    const resource = { values: [values] };
+
+    try {
+        const res = await sheets.spreadsheets.values.append({
+            spreadsheetId,
+            range,
+            valueInputOption,
+            resource,
+        });
+        return res; // Returns the response from the Sheets API
+    } catch (error) {
+        console.error('error', error); // Logs errors
+    }
+}
+
+export async function readSheet(range) {
+    const spreadsheetId = process.env.SPREADSHEET_ID
+    const sheets = google.sheets({
+        version: 'v4', auth
+    });
+
+    try {
+        const response = await sheets.spreadsheets.values.get({
+            spreadsheetId, range
+        });
+        const rows = response.data.values; // Extracts the rows from the response.
+        return rows; // Returns the rows.
+    } catch (error) {
+        console.error('error', error); // Logs errors.
+    }
+}
